e58e714c0335b589cca1566fb9b20c2745a350fc
Shashank Chhoker <shashanksaad07@gmail.com>
2025-05-18 17:32:23 +0530

    add params in data ingestion

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 import logging
-
+import yaml
 
 #Ensure that the Log Directory Exits 
 log_dir = "logs"
@@ -31,6 +31,24 @@ console_handeler.setFormatter(formatter)
 
 logger.addHandler(console_handeler)
 logger.addHandler(file_handeler)
+
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 
 #Important function for the Loading the Data From the Source :
@@ -83,10 +101,12 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 # Main function
 def main():
+    params = load_params(params_path='params.yaml')
+    test_size = params['data_ingestion']['test_size']
     data_url = "https://raw.githubusercontent.com/Shashank975/Mlops-Pipeline-Dvc-Mlflow/refs/heads/main/Experiments/spam.csv"
     df = load_data(data_url)
     final_df = preprocess_data(df)
-    train_data, test_data = train_test_split(final_df, test_size=0.2, random_state=2)
+    train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=2)
     save_data(train_data, test_data, data_path='./data')
 
 if __name__ == "__main__":
